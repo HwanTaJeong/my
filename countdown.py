f97375cdb52b756d51860be86bc3a580fff81d66
HwanTaJeong <128822068+HwanTaJeong@users.noreply.github.com>
2023-12-02 15:30:09 +0900

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,17 @@
+from time import sleep
+import winsound
+
+def countdown(t):
+   
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        winsound.Beep(450, 750)
+        sleep(0.25)
+        t -= 1
+
+    print('00:00')
+    winsound.Beep(900, 1000)
+ 
+countdown(5)
