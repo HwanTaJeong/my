216e7876c517b1346a166c4b4e52ef5c0fd61f89
HwanTaJeong <128822068+HwanTaJeong@users.noreply.github.com>
2024-02-26 22:26:45 +0900

    countdown.py 업데이트

--- countdown.py
+++ countdown.py
@@ -4,12 +4,12 @@ import winsound #import winsound
 def countdown(t):
    
     while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        winsound.Beep(450, 750)
-        sleep(0.25)
-        t -= 1
+        mins, secs = divmod(t, 60) #define minutes, seconds
+        timer = '{:02d}:{:02d}'.format(mins, secs) #define timer
+        print(timer, end="\r") #reset timer
+        winsound.Beep(450, 750) #play sound
+        sleep(0.25) #give some delay
+        t -= 1 #counting time
 
     print('00:00')
     winsound.Beep(900, 1000)
